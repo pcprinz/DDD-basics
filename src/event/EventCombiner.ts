502c242878f3affa63e0a73c1fd37d1fcd9d70ff
Christian Prinz <pcprinz@outlook.de>
2022-11-30 22:36:16 +0100

    updated EventCombiner docs

--- src/event/EventCombiner.ts
+++ src/event/EventCombiner.ts
@@ -1,9 +1,10 @@
 import { DomainEvent } from './DomainEvent';
+import { AllThen, Consume, Once, SomeThen } from './EventCombiner.type';
 import { EventHandler } from './EventHandler';
 
 /** ### Combines multiple events to let subscribers listen to any combination of the events
  *
- * There are 6 different combinations that can be achieved by the following chaining of the EventCombiners methods.
+ * There are 8 different combinations that can be achieved by the following chaining of the EventCombiners methods.
  *
  * | method                      | first call on  | subsequent calls on    | provided payloads of                 |
  * |:----------------------------|:---------------|:-----------------------|:-------------------------------------|
@@ -47,70 +48,70 @@ import { EventHandler } from './EventHandler';
  * Consider having `new EventCombiner('myCombiner')` as a prefix for the following method chains:
  *
  * ##### `.all(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **all** of the given HANDLERS have fired.
- * - After that, every time one of the HANDLERS fires, the CALLBACK is called again.
- * - *use this if you want to react to multiple events, that all* **must** *be fired once*
+ * - Calls the given CALLBACK once **all** of the given HANDLERS have dispatched.
+ * - After that, every time one of the HANDLERS dispatches, the CALLBACK is called again.
+ * - *use this if you want to react to multiple events, that all* **must** *be dispatched once*
  *
  * ##### `.some(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **one** of the given HANDLERS has fired.
- * - After that, every time one of the HANDLERS fires, the CALLBACK is called again.
+ * - Calls the given CALLBACK once **one** of the given HANDLERS has dispatched.
+ * - After that, every time one of the HANDLERS dispatches, the CALLBACK is called again.
  * - *use this to react to multiple events without any restriction*
  *
  * ##### `.once().all(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **all** of the given HANDLERS have fired.
- * - After that, the CALLBACK is never called again, when one of the HANDLERS is fired.
- * - *use this if you want to react when a whole set of events has fired once*
+ * - Calls the given CALLBACK once **all** of the given HANDLERS have dispatched.
+ * - After that, the CALLBACK is never called again, when one of the HANDLERS is dispatched.
+ * - *use this if you want to react when a whole set of events has dispatched once*
  *
  * ##### `.once().all(HANDLERS).first().then(CALLBACK)`
- * - Calls the given CALLBACK once **all** of the given HANDLERS have fired.
- * - After that, the CALLBACK is never called again, when one of the HANDLERS is fired.
+ * - Calls the given CALLBACK once **all** of the given HANDLERS have dispatched.
+ * - After that, the CALLBACK is never called again, when one of the HANDLERS is dispatched.
  * - Different to once.all just the first occurred event for every handler is provided
- * - *use this if you want to react when a whole set of events has fired once*
+ * - *use this if you want to react when a whole set of events has dispatched once*
  *
  * ##### `.once().some(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **one** of the given HANDLERS has fired.
- * - After that, the CALLBACK is never called again, when one of the HANDLERS is fired.
- * - *use this if you want to react if a random event fires once*
+ * - Calls the given CALLBACK once **one** of the given HANDLERS has dispatched.
+ * - After that, the CALLBACK is never called again, when one of the HANDLERS is dispatched.
+ * - *use this if you want to react if a random event dispatches once*
  *
  * ##### `.consume().all(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **all** of the given HANDLERS have fired.
- * - After that **all** of the given HANDLERS have to be fired once **again** for another CALLBACK call.
+ * - Calls the given CALLBACK once **all** of the given HANDLERS have dispatched.
+ * - After that **all** of the given HANDLERS have to be dispatched once **again** for another CALLBACK call.
  * - *Use this if you want to react to a synchronously repeated occurrence of a whole set of events*
  *
  * ##### `.consume().all(HANDLERS)first().then(CALLBACK)`
- * - Calls the given CALLBACK once **all** of the given HANDLERS have fired.
- * - After that **all** of the given HANDLERS have to be fired once **again** for another CALLBACK call.
+ * - Calls the given CALLBACK once **all** of the given HANDLERS have dispatched.
+ * - After that **all** of the given HANDLERS have to be dispatched once **again** for another CALLBACK call.
  * - Different to consume.all just the first occurred event for every handler is provided
  * - *Use this if you want to react to a synchronously repeated occurrence of a whole set of events*
  *
  * ##### `.consume().some(HANDLERS).then(CALLBACK)`
- * - Calls the given CALLBACK once **one** of the given HANDLERS has fired.
- * - After that, every time one of the HANDLERS fires, the CALLBACK is called again.
+ * - Calls the given CALLBACK once **one** of the given HANDLERS has dispatched.
+ * - After that, every time one of the HANDLERS dispatches, the CALLBACK is called again.
  * - *use this to react to multiple events with the restriction, that only the last occurred event
  *   has a payload in the CALLBACK*
  *
  * #### `consume()` behavior
- * "consume" can be understood as a consumption of the fired events when the callback is called, so
+ * "consume" can be understood as a consumption of the dispatched events when the callback is called, so
  * `consume()` resets the received events. This means that the payloads provided in the CALLBACK will
  * only be the ones that have been delivered since the last call of the CALLBACK. Therefore `all()`
  * will have all "*fresh*" payloads and `some()` will have only one (the last) payload.
  *
  * Without `consume()` the CALLBACKS will always receive the respectively last payload that has been
- * delivered for every corresponding event - even if the event has fired in a previous iteration
+ * delivered for every corresponding event - even if the event has dispatched in a previous iteration
  * of the CALLBACK.
  *
  * #### HANDLERS
  * HANDLERS is a list of `EventHandler`s that one would normally subscribe separately to.
  *
  * #### CALLBACK
- * The CALLBACK is a function that provides the fired `DomainEvent`s from the given HANDLERS as an array. The order of the DomainEvents
+ * The CALLBACK is a function that provides the dispatched `DomainEvent`s from the given HANDLERS as an array. The order of the DomainEvents
  * in the array correlates to the order of the given HANDLERS from the `all(HANDLERS)` or `some(HANDLERS)` method.
  * The DomainEvents including their payloads are also correctly typed corresponding to their HANDLERS.
  *
  * ##### ATTENTION:
- * Since `some()` means that not all of the HANDLERS have to fire, not all of the provided
+ * Since `some()` means that not all of the HANDLERS have to dispatch, not all of the provided
  * DomainEvents may exist. Therefore the type is `DomainEvent<any> | 'pending'`, where 'pending' means,
- * that the EVENT has not yet been fired. This has to be catched when working with the `.some(HANDLERS)` payloads.
+ * that the EVENT has not yet been dispatched. This has to be catched when working with the `.some(HANDLERS)` payloads.
  */
 export class EventCombiner {
   protected _name: string;
@@ -122,6 +123,8 @@ export class EventCombiner {
   private _consume: boolean = false;
   private _first: boolean = false;
 
+  // CONSTRUCTION + SUBSCRIPTION ___________________________________________________________________
+
   constructor(name: string) {
     this._name = name;
   }
@@ -132,75 +135,75 @@ export class EventCombiner {
    * no listening will happen, but the extendable `onLog()` method will be called with
    * `type = 'alreadyDestroyed'`
    */
-  public destroy() {
+  destroy(): void {
     this.unsubscribeEventHandlers();
     this._destroyed = true;
   }
 
-  /**
-   * ### When all given events occur ...
-   * (every given {@link EventHandler} has dispatched at least one {@link DomainEvent})
-   * @chains {@link then} & ({@link first} if chained with {@link consume})
-   */
-  all = <EH extends EventHandler<any>[]>(...eventHandler: [...EH]): AllThenType<EH> => {
-    this._eventHandlers = eventHandler;
-    this._all = true;
+  private unsubscribeEventHandlers(): void {
+    this._eventHandlers.forEach((handler) => handler.unsubscribe(this._name));
+    this.onLog('destroy');
+  }
 
-    // @ts-ignore STATIC TYPE CONVERSION
-    return { then: this.then, first: this.first };
-  };
+  // MODIFIERS _____________________________________________________________________________________
 
-  /** ### When one of the given events occurs ...
-   * (just one of the given {@link EventHandler} has dispatched a {@link DomainEvent})
-   * @chains {@link then}
-   */
-  some = <EH extends EventHandler<any>[]>(...eventHandler: [...EH]): SomeThenType<EH> => {
-    this._eventHandlers = eventHandler;
-
-    // @ts-ignore STATIC TYPE CONVERSION
-    return { then: this.then };
-  };
-
-  /** ### Just the first time ...
+  /** ### `Just the first time ...`
    * (and never again)
    * @chains {@link all} & {@link some}
    */
-  once = (): {
-    all: <EH extends EventHandler<any>[]>(
-      ...eventHandler_0: EH
-    ) => AllThenType<EH> & FirstThenType<EH>;
-    some: <EH extends EventHandler<any>[]>(...eventHandler_0: EH) => SomeThenType<EH>;
-  } => {
+  once = (): Once => {
     this._once = true;
 
     // @ts-ignore STATIC TYPE CONVERSION
     return { all: this.all, some: this.some };
   };
 
-  /** ### Every time ...
-   * (`all.consume` means: always every {@link EventHandler} again)
+  /** ### `Every time ...`
+   * (`consume().all()` means: always every {@link EventHandler} must dispatch again)
    * @chains {@link all} & {@link some}
    */
-  consume = (): {
-    all: <EH extends EventHandler<any>[]>(
-      ...eventHandler_0: EH
-    ) => AllThenType<EH> & FirstThenType<EH>;
-    some: <EH extends EventHandler<any>[]>(...eventHandler_0: EH) => SomeThenType<EH>;
-  } => {
+  consume = (): Consume => {
     this._consume = true;
 
     // @ts-ignore STATIC TYPE CONVERSION
     return { all: this.all, some: this.some };
   };
 
-  private first = <EH extends EventHandler<any>[]>(): AllThenType<EH> => {
-    this._first = true;
+  /** ### `Always when one of the given handlers dispatches ...`
+   * (just one of the given {@link EventHandler} has dispatched a {@link DomainEvent})
+   * @chains {@link then}
+   */
+  some = <Handlers extends EventHandler<any>[]>(...handlers: [...Handlers]): SomeThen<Handlers> => {
+    this._eventHandlers = handlers;
 
     // @ts-ignore STATIC TYPE CONVERSION
     return { then: this.then };
   };
 
-  private then = (callback: (events: (DomainEvent<any> | 'pending')[]) => void) => {
+  /**
+   * ### `When all given handlers dispatch (and then on every event) ...`
+   * (every given {@link EventHandler} has dispatched at least one {@link DomainEvent})
+   * @chains {@link then}
+   */
+  all = <Handlers extends EventHandler<any>[]>(...handlers: [...Handlers]): AllThen<Handlers> => {
+    this._eventHandlers = handlers;
+    this._all = true;
+
+    // @ts-ignore STATIC TYPE CONVERSION
+    return { then: this.then, first: this.first };
+  };
+
+  private first = <Handlers extends EventHandler<any>[]>(): AllThen<Handlers> => {
+    this._first = this._consume || this._once; // otherwise first is not allowed and should not work
+    // TS users will not see this method, but JS users can force using this with combiner.all().first().then(...)
+
+    // @ts-ignore STATIC TYPE CONVERSION
+    return { then: this.then };
+  };
+
+  // THEN __________________________________________________________________________________________
+
+  private then = (callback: (events: (DomainEvent<any> | 'pending')[]) => void): void => {
     this._destroyed && this.onLog('alreadyDestroyed');
     this._eventHandlers?.forEach((handler) => {
       // set empty events
@@ -229,21 +232,6 @@ export class EventCombiner {
     });
   };
 
-  /** This method is called when the `.then(...)` callback is called, the combiner is destroyed,
-   * or the callback is called, when already destroyed. The original method will do nothing.
-   * To utilize this method (eg. for a Logger integration) this class must be extended:
-   *
-   * @example
-   * class MyEventCombiner extends EventCombiner {
-   *   protected onLog(type: 'then' | 'destroy' | 'alreadyDestroyed'): void {
-   *     console.log(type, this._name, this._receivedEvents, this._eventHandlers);
-   *   }
-   * }
-   */
-  protected onLog(type: 'then' | 'destroy' | 'alreadyDestroyed') {
-    // extend this class to implement this function in order to inject logging
-  }
-
   private thenIsFulfilled(): boolean {
     for (const event of this._receivedEvents.values()) {
       if (this._all && event === 'pending') {
@@ -257,52 +245,25 @@ export class EventCombiner {
     return this._all; // ===   all & no event pending   ||  !all (some) & every event pending
   }
 
-  private unsubscribeEventHandlers(): void {
-    this._eventHandlers.forEach((handler) => handler.unsubscribe(this._name));
-    this.onLog('destroy');
-  }
-
   private resetReceivedEvents(): void {
     [...this._receivedEvents.keys()].forEach((handlerName) => {
       this._receivedEvents.set(handlerName, 'pending');
     });
   }
+
+  /** ### Inject Logging (by extending this class)
+   *  This method is called when the `.then(...)` callback is called, the combiner is destroyed,
+   * or the callback is called, when already destroyed. The original method will do nothing.
+   * To utilize this method (eg. for a Logger integration) this class must be extended:
+   *
+   * @example
+   * class MyEventCombiner extends EventCombiner {
+   *   protected onLog(type: 'then' | 'destroy' | 'alreadyDestroyed'): void {
+   *     console.log(type, this._name, this._receivedEvents, this._eventHandlers);
+   *   }
+   * }
+   */
+  protected onLog(type: 'then' | 'destroy' | 'alreadyDestroyed'): void {
+    // extend this class to implement this function in order to inject logging
+  }
 }
-
-export type FirstThenType<Handler> = {
-  /**
-   * ### Just take the first occurred events and ...
-   * (if an event already occurred, then it will not be overwritten by following events of the same
-   * type until the `then()` is called and the events are consumed)
-   * @chains {@link then}
-   */
-  first: () => AllThenType<Handler>;
-};
-
-export type AllThenType<Handler> = {
-  /** ### ... Call the given callback.
-   *
-   * A list of all possible {@link DomainEvent}s is passed to the callback,
-   * which corresponds to the order of the defined {@link EventHandler}s.
-   */
-  then: (
-    callback: (events: {
-      [K in keyof Handler]: Handler[K] extends EventHandler<infer X> ? DomainEvent<X> : never;
-    }) => void
-  ) => void;
-};
-
-export type SomeThenType<EH> = {
-  /** ### ... Call the given callback.
-   *
-   * A list of all possible {@link DomainEvent}s is passed to the callback,
-   * which corresponds to the order of the defined {@link EventHandler}s.
-   *
-   * ### Since this method is chained with a `some(...)` method, the {@link DomainEvent}s may also be `'pending'` for those {@link EventHandler}s which have not fired yet.
-   */
-  then: (
-    callback: (events: {
-      [K in keyof EH]: EH[K] extends EventHandler<infer X> ? DomainEvent<X> | 'pending' : never;
-    }) => void
-  ) => void;
-};
